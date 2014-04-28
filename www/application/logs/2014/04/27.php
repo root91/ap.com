5a03f29503ab4ee181ecad664d938c6e456bf18b
Dmitrij <perestadmitrij@mail.ru>
2014-04-28 20:15:04 +0300

    First Commit
    It's first commit

---
+++ www/application/logs/2014/04/27.php
@@ -0,0 +1,44 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2014-04-27 22:32:37 --- CRITICAL: ReflectionException [ 0 ]: Class Controller_Portfolio_Portfolio does not have a constructor, so you cannot pass any constructor arguments ~ SYSPATH\classes\Kohana\Request\Client\Internal.php [ 94 ] in Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php:94
+2014-04-27 22:32:37 --- DEBUG: #0 Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php(94): ReflectionClass->newInstance(Object(Request), Object(Response))
+#1 Z:\home\ap.com\www\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#2 Z:\home\ap.com\www\system\classes\Kohana\Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#3 Z:\home\ap.com\www\index.php(118): Kohana_Request->execute()
+#4 {main} in Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php:94
+2014-04-27 22:33:03 --- CRITICAL: ReflectionException [ 0 ]: Class Controller_Portfolio_Portfolio does not have a constructor, so you cannot pass any constructor arguments ~ SYSPATH\classes\Kohana\Request\Client\Internal.php [ 94 ] in Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php:94
+2014-04-27 22:33:03 --- DEBUG: #0 Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php(94): ReflectionClass->newInstance(Object(Request), Object(Response))
+#1 Z:\home\ap.com\www\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#2 Z:\home\ap.com\www\system\classes\Kohana\Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#3 Z:\home\ap.com\www\index.php(118): Kohana_Request->execute()
+#4 {main} in Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php:94
+2014-04-27 22:33:22 --- CRITICAL: ReflectionException [ 0 ]: Class Controller_Portfolio_Portfolio does not have a constructor, so you cannot pass any constructor arguments ~ SYSPATH\classes\Kohana\Request\Client\Internal.php [ 94 ] in Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php:94
+2014-04-27 22:33:22 --- DEBUG: #0 Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php(94): ReflectionClass->newInstance(Object(Request), Object(Response))
+#1 Z:\home\ap.com\www\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#2 Z:\home\ap.com\www\system\classes\Kohana\Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#3 Z:\home\ap.com\www\index.php(118): Kohana_Request->execute()
+#4 {main} in Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php:94
+2014-04-27 22:33:45 --- CRITICAL: ReflectionException [ 0 ]: Class Controller_Portfolio_Portfolio does not have a constructor, so you cannot pass any constructor arguments ~ SYSPATH\classes\Kohana\Request\Client\Internal.php [ 94 ] in Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php:94
+2014-04-27 22:33:45 --- DEBUG: #0 Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php(94): ReflectionClass->newInstance(Object(Request), Object(Response))
+#1 Z:\home\ap.com\www\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#2 Z:\home\ap.com\www\system\classes\Kohana\Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#3 Z:\home\ap.com\www\index.php(118): Kohana_Request->execute()
+#4 {main} in Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php:94
+2014-04-27 22:34:07 --- CRITICAL: ReflectionException [ 0 ]: Class Controller_Portfolio_Portfolio does not have a constructor, so you cannot pass any constructor arguments ~ SYSPATH\classes\Kohana\Request\Client\Internal.php [ 94 ] in Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php:94
+2014-04-27 22:34:07 --- DEBUG: #0 Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php(94): ReflectionClass->newInstance(Object(Request), Object(Response))
+#1 Z:\home\ap.com\www\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#2 Z:\home\ap.com\www\system\classes\Kohana\Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#3 Z:\home\ap.com\www\index.php(118): Kohana_Request->execute()
+#4 {main} in Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php:94
+2014-04-27 22:34:49 --- CRITICAL: View_Exception [ 0 ]: The requested view template could not be found ~ SYSPATH\classes\Kohana\View.php [ 257 ] in Z:\home\ap.com\www\system\classes\Kohana\View.php:137
+2014-04-27 22:34:49 --- DEBUG: #0 Z:\home\ap.com\www\system\classes\Kohana\View.php(137): Kohana_View->set_filename('template')
+#1 Z:\home\ap.com\www\system\classes\Kohana\View.php(30): Kohana_View->__construct('template', NULL)
+#2 Z:\home\ap.com\www\system\classes\Kohana\Controller\Template.php(33): Kohana_View::factory('template')
+#3 Z:\home\ap.com\www\application\classes\mycontroller.php(6): Kohana_Controller_Template->before()
+#4 Z:\home\ap.com\www\system\classes\Kohana\Controller.php(69): Mycontroller->before()
+#5 [internal function]: Kohana_Controller->execute()
+#6 Z:\home\ap.com\www\system\classes\Kohana\Request\Client\Internal.php(97): ReflectionMethod->invoke(Object(Controller_Portfolio_Portfolio))
+#7 Z:\home\ap.com\www\system\classes\Kohana\Request\Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#8 Z:\home\ap.com\www\system\classes\Kohana\Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#9 Z:\home\ap.com\www\index.php(118): Kohana_Request->execute()
+#10 {main} in Z:\home\ap.com\www\system\classes\Kohana\View.php:137
